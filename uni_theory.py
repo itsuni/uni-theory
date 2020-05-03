9b3e5ae36b732a1b9ae287293a29088ae8d75ea0
Gian Esteves <31361395+itsuni@users.noreply.github.com>
2020-05-03 11:54:03 -0400

    complete

---
+++ uni_theory.py
@@ -0,0 +1,23 @@
+def theory(n):
+    a = n+1
+    b = n-1
+
+    print(n*n)
+    print(a*b)
+    print("")
+
+    if a*b == n*n-1:
+        return('pass')
+    else:
+        return('fail')
+
+def test():
+    for i in range(999):
+        
+        if theory(i) == 'fail':
+            print("FAIL")
+            break
+    
+    print(f"Numbers 0 - {i} are ok!")
+
+test()
